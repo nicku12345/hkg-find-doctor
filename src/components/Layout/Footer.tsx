476bcb21e1d9a287b1d8ea57ffd3cbcb7a0421f1
Nick Liu <nicku5011@gmail.com>
2025-07-17 22:16:48 +0800

    lgtm

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-700 text-white py-2 sticky bottom-0">
         <div className="container mx-auto px-4 flex justify-end items-center">
-            <p className="mr-4">nicku123</p>
+            <p className="mr-4">Group 38</p>
         </div>
     </footer>
   )
